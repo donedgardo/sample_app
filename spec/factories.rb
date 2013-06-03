4a031c131b6205f4ec26914035a0eb01876104c1
Edgardo <edgardo.g.carreras@gmail.com>
2013-06-03 13:10:05 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name    "Edgardo G. Carreras"
+    email   "edgardo.g.carreras@gmail.com"
+    password "semenanal"
+    password_confirmation "semenanal"
+  end
+end
